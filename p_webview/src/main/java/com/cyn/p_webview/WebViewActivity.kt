7276e51b7ef97e45ff13d1c56dfc5984eb42f629
chen <983098760@qq.com>
2023-03-14 01:10:08 +0800

    完成初步webview页面加载

--- p_webview/src/main/java/com/cyn/p_webview/WebViewActivity.kt
+++ p_webview/src/main/java/com/cyn/p_webview/WebViewActivity.kt
@@ -3,15 +3,23 @@ package com.cyn.p_webview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.cyn.p_webview.databinding.ActivityWebviewBinding
 
 class WebViewActivity : AppCompatActivity() {
+    private var mBinding :ActivityWebviewBinding? = null
+    private var mUri : String = ""
     companion object{
         const val URL = "url"
         const val TAG = "WebViewActivity"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_webview)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_webview)
+        mUri = intent?.extras?.getString(URL) ?: "https://www.baidu.com"
+        mBinding?.webview?.loadUrl(mUri)
+        mBinding?.webview?.settings?.javaScriptEnabled = true
         Log.e(TAG, "url: ${intent.extras?.getString(URL).toString()}")
+
     }
 }
